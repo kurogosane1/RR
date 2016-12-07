a4873df1fb6ad7c2da898c2f5fd00d753110675b
Suzanne Jacobs <Suzanne Jacobs>
2016-12-06 18:16:54 -0600

    added html for boxes

---
+++ rr.js
@@ -0,0 +1,8 @@
+// Front Page
+// Ask user if they want to use their location
+// Initialize multiple API's based on location
+// Display nearest popular activities based on location
+// User can select more options and will be directed to page with advanced search options
+// 
+
+ 
